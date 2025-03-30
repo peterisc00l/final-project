db908b5008087d6426bee8105164bfee4d25a093
peterisc00l <augustambaekwe@gmail.com>
2025-03-30 14:02:50 +0100

    Add files via upload

---
+++ # Printing a greetin message.py
@@ -0,0 +1,11 @@
+# Printing a greetin message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Peter")
+
+# Printing a fun fact about myself
+print("I love football.")
+
+# Printing and printing a simple claculation
+print("The result of 3 + 5 is:", 3 + 5)
